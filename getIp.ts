909289c4359ab683f8d0f56a3b78083ccc4a5569
xna <xna00@qq.com>
2023-02-04 15:22:21 +0800

    update

--- getIp.ts
+++ getIp.ts
@@ -1,7 +1,12 @@
 import os from 'os';
 
 function getLocalIp() {
-  const res = Object.values(os.networkInterfaces()).flat().filter(item => item && !item.internal && (item.address.startsWith('192.') || item.address.startsWith('10.')));
+  const t = os.networkInterfaces()
+  const res = Object.keys(t)
+    .filter(k => !k.includes('rmnet'))
+    .map(k => t[k])
+    .flat()
+    .filter(item => item && !item.internal && (item.address.startsWith('192.') || item.address.startsWith('10.')));
   return res[0]?.address;
 }
 
